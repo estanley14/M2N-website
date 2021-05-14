b1f67aff67983c3f75331be4450597ce376ee875
codyporter88 <codywporter@gmail.com>
2021-05-14 11:32:58 -0600

    image updates to side nav component

--- src/components/sideNav.js
+++ src/components/sideNav.js
@@ -4,6 +4,13 @@ import { Link } from "gatsby"
 
 // IMAGES
 import vector4942x from "../import/img/vector-494@2x.png"
+import ln9732x from "../import/img/line-973@2x.png"
+import vctrStrk2x from "../import/img/vector--stroke-@2x.png" 
+import vctrStrk12x from "../import/img/vector--stroke--1@2x.png"
+import ln97351x from "../import/img/line-973-5@1x.png"
+import vctrStrk22x from "../import/img/vector--stroke--2@2x.png"
+import vctrStrk32x from "../import/img/vector--stroke--3@2x.png"
+
 
 const SideNav = ({ setNavState }) => {
     return (
@@ -16,7 +23,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-zNiu1U"></div>
                     <h1 className="home-zNiu1U">HOME</h1>
                     {/* <div className="indicator-zNiu1U smart-layers-pointers"></div> */}
-                    <img className="line-973-zNiu1U" src="./import/img/line-973@2x.png" />
+                    <img className="line-973-zNiu1U" src={ln9732x} />
                 </div>
             </Link>
             <Link to="/mpower">
@@ -24,7 +31,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-FljbKi"></div>
                     <div className="mpower-FljbKi dmsans-bold-gunsmoke-24px">MPOWER</div>
                     {/* <div className="indicator-FljbKi smart-layers-pointers"></div> */}
-                    <img className="line-973-FljbKi" src="./import/img/line-973@2x.png" />
+                    <img className="line-973-FljbKi" src={ln9732x} />
                 </div>
             </Link>
             <Link to="/company">
@@ -32,7 +39,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-PiyUW5"></div>
                     <div className="company-PiyUW5 dmsans-bold-gunsmoke-24px">COMPANY</div>
                     {/* <div className="indicator-PiyUW5 smart-layers-pointers"></div> */}
-                    <img className="line-973-PiyUW5" src="./import/img/line-973@2x.png" />
+                    <img className="line-973-PiyUW5" src={ln9732x} />
                 </div>
             </Link>
             <Link to="/partners">
@@ -40,7 +47,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-Qtr366"></div>
                     <div className="partners-Qtr366 dmsans-bold-gunsmoke-24px">PARTNERS</div>
                     {/* <div className="indicator-Qtr366 smart-layers-pointers"></div> */}
-                    <img className="line-973-Qtr366" src="./import/img/line-973@2x.png" />
+                    <img className="line-973-Qtr366" src={ln9732x} />
                 </div>
             </Link>
             <Link to="/mpower-app">
@@ -48,12 +55,12 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-DMZGJ0"></div>
                     <div className="login-sign-up-DMZGJ0 dmsans-bold-gunsmoke-24px">LOGIN / SIGN UP</div>
                     {/* <div className="indicator-DMZGJ0 smart-layers-pointers"></div> */}
-                    <img className="line-973-DMZGJ0" src="./import/img/line-973@2x.png" />
+                    <img className="line-973-DMZGJ0" src={ln9732x} />
                 </div>
             </Link>
             <div className="x-C61RwL" style={{cursor: "pointer"}} onClick={()=> setNavState(false)}>
-                <img className="vector-stroke-412M3r" src="./import/img/vector--stroke-@2x.png" />
-                <img className="vector-stroke-pr9M1i" src="./import/img/vector--stroke--1@2x.png" />
+                <img className="vector-stroke-412M3r" src={vctrStrk2x} />
+                <img className="vector-stroke-pr9M1i" src={vctrStrk12x} />
             </div>
             </div>
             <div className="mobile-nav-720px screen">
@@ -63,7 +70,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-zNiu1U"></div>
                     <div className="home-zNiu1U">HOME</div>
                     {/* <div className="indicator-zNiu1U smart-layers-pointers"></div> */}
-                    <img className="line-973-zNiu1U" src="./import/img/line-973-5@1x.png" />
+                    <img className="line-973-zNiu1U" src={ln97351x} />
                 </div>
             </Link>
             <Link to="/mpower">
@@ -71,7 +78,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-FljbKi"></div>
                     <div className="mpower-FljbKi dmsans-bold-gunsmoke-24px">MPOWER</div>
                     {/* <div className="indicator-FljbKi smart-layers-pointers"></div> */}
-                    <img className="line-973-FljbKi" src="./import/img/line-973-5@1x.png" />
+                    <img className="line-973-FljbKi" src={ln97351x} />
                 </div>
             </Link>
             <Link to="/company">
@@ -79,7 +86,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-PiyUW5"></div>
                     <div className="company-PiyUW5 dmsans-bold-gunsmoke-24px">COMPANY</div>
                     {/* <div className="indicator-PiyUW5 smart-layers-pointers"></div> */}
-                    <img className="line-973-PiyUW5" src="./import/img/line-973-5@1x.png" />
+                    <img className="line-973-PiyUW5" src={ln97351x} />
                 </div>
             </Link>
             <Link to="/partners">
@@ -87,7 +94,7 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-Qtr366"></div>
                     <div className="partners-Qtr366 dmsans-bold-gunsmoke-24px">PARTNERS</div>
                     {/* <div className="indicator-Qtr366 smart-layers-pointers"></div> */}
-                    <img className="line-973-Qtr366" src="./import/img/line-973-5@1x.png" />
+                    <img className="line-973-Qtr366" src={ln97351x} />
                 </div>
             </Link>
             <Link to="/mpower-app">
@@ -95,12 +102,12 @@ const SideNav = ({ setNavState }) => {
                     <div className="textframe-DMZGJ0"></div>
                     <div className="login-sign-up-DMZGJ0 dmsans-bold-gunsmoke-24px">LOGIN / SIGN UP</div>
                     {/* <div className="indicator-DMZGJ0 smart-layers-pointers"></div> */}
-                    <img className="line-973-DMZGJ0" src="./import/img/line-973-5@1x.png" />
+                    <img className="line-973-DMZGJ0" src={ln97351x} />
                 </div>
             </Link>
             <div className="x-C61RwL" style={{cursor: "pointer"}} onClick={()=> setNavState(false)}>
-                <img className="vector-stroke-412M3r" src="./import/img/vector--stroke--2@2x.png" />
-                <img className="vector-stroke-pr9M1i" src="./import/img/vector--stroke--3@2x.png" />
+                <img className="vector-stroke-412M3r" src={vctrStrk22x} />
+                <img className="vector-stroke-pr9M1i" src={vctrStrk32x} />
             </div>
             </div>
         </div>
