70eed72269323c847fceb85d925686dccbcc3f0d
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-05-21 00:43:33 -0500

    Updated Organization Page
    Added content and styling for CTA section. Still needs solution for the linear gradient border, and also tie the button to the correct route with Link.

--- src/components/organization/index.js
+++ src/components/organization/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './HeroSection'
 import OrganizationsSection from './OrganizationsSection'
+import CTASection from './CTASection'
 
 export default function OrganizationPage() {
     return (
         <div>
             <HeroSection />
             <OrganizationsSection />
+            <CTASection />
         </div>
     )
 }
