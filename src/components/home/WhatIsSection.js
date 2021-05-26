b91df769d29f1296f6caafda98461b17f3f99251
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-05-26 07:33:57 -0500

    Commit

--- src/components/home/WhatIsSection.js
+++ src/components/home/WhatIsSection.js
@@ -3,11 +3,23 @@ import styled from 'styled-components'
 import { IconButton } from '../reusable-components/'
 
 const StyledSection = styled.section`
-    border: 2px dashed black;
+    padding: 0px 16px 0px 16px;
+    background: #191C23;
+    border: 2px dashed white;
+
+    & > .demo {
+        background: #416FF4;
+        width: 100%;
+        height: 272px;
+        border: 2px dashed orange;
+    }
 `
 export default function WhatIsSection() {
     return (
         <StyledSection>
+            <div className={'demo'}>
+                <p>demo div</p>
+            </div>
             <IconButton 
                 label={'View benefits'} 
                 color={'blue'}
