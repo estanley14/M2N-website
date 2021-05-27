0bb978a49075dc6693ce614ba9e25e653f26636c
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-05-27 00:36:05 -0500

    Commit

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,7 +3,6 @@ import HeroSection from './HeroSection'
 import NetworkSection from './NetworkSection'
 import PlatformSection from './PlatformSection'
 import WhatIsSection from './WhatIsSection'
-import LearnMoreSection from './LearnMoreSection'
 
 export default function HomePage() {
     return (
@@ -12,7 +11,6 @@ export default function HomePage() {
             <NetworkSection />
             <PlatformSection />
             <WhatIsSection />
-            <LearnMoreSection />
         </div>
     )
 }
