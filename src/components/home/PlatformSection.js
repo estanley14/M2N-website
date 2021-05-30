0bfba7644261003910809b1044b12e060224ff73
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-05-30 11:01:55 -0500

    Commit

--- src/components/home/PlatformSection.js
+++ src/components/home/PlatformSection.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import { PlatformCard1, PlatformCard2, PlatformCard3 } from '../reusable-components/'
 import mobileGraph from '../../assets/images/home-platform-graphic.png'
+import card1Mobile from '../../assets/images/home/platform-card1-small.png'
+import card2Mobile from '../../assets/images/home/platform-card2-small.png'
+import card3Mobile from '../../assets/images/home/platform-card3-small.png'
 
 const StyledSection = styled.section`
     position: relative;
@@ -23,6 +26,12 @@ const StyledSection = styled.section`
         align-items: center;
         gap: 24px;
         // border: 2px solid black;
+
+        & > img {
+            // width: 312px;
+            // height: 180px;
+            // border: 1px dashed orange;
+        }
     }
 `
 
@@ -35,9 +44,24 @@ export default function PlatformSection() {
                 className={'graph-image'}
             />
             <div className={'card-container'}>
-                <PlatformCard1 title={'Membership'} body={'Builds an active network of diverse professionals empowered by their individual lived experiences through personal & professional development.'} />
+                <div className={'image'}></div>
+                <img
+                    src={card1Mobile}
+                    alt={'membership card'}
+                />
+
+                <img
+                    src={card2Mobile}
+                    alt={'placement card'}
+                />
+
+                <img
+                    src={card3Mobile}
+                    alt={'services card'}
+                />
+                {/* <PlatformCard1 title={'Membership'} body={'Builds an active network of diverse professionals empowered by their individual lived experiences through personal & professional development.'} />
                 <PlatformCard2 title={'Placement'} body={'Internal recruiting services engage a network of companies committed to recruiting, onboarding, and mentoring M2N membership.'} />
-                <PlatformCard3 title={'Services'} body={'Supports transformation change at the systematic level through DEI support and services lead by industry leaders.'} />
+                <PlatformCard3 title={'Services'} body={'Supports transformation change at the systematic level through DEI support and services lead by industry leaders.'} /> */}
             </div>
         </StyledSection>
     )
