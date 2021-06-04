1f92f652fd936f38ee151adfdf4e2d017010b1f8
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-04 10:29:04 -0500

    Commit

--- src/components/home/WhatIsSection.js
+++ src/components/home/WhatIsSection.js
@@ -2,10 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 // import { navigate } from 'gatsby'
 import { BenefitsList } from './BenefitsList'
-import productImage from '../../assets/images/home/whatis-image-mobile.png'
-import backgroundStripesMobile from '../../assets/images/home/whatis-stripes-mobile.svg'
+import productImageMobile from '../../assets/images/home/whatis-image-mobile.png'
+import productImageDesktop from '../../assets/images/home/whatis-image-desktop.png'
+import stripesMobile from '../../assets/images/home/whatis-stripes-mobile.svg'
+import stripesDesktop from '../../assets/images/home/whatis-stripes-desktop.svg'
 import redOvalMobile from '../../assets/images/home/whatis-redoval-mobile.svg'
+
 import grayOvalMobile from '../../assets/images/home/whatis-grayoval-mobile.svg'
+import grayOvalDesktop from '../../assets/images/home/whatis-grayoval-desktop.svg'
+
+import logoMobile from '../../assets/images/home/whatis-logo-mobile.png'
+import logoDesktop from '../../assets/images/home/whatis-logo-desktop.png'
+import dotsMobile from '../../assets/images/home/whatis-dots-mobile.svg'
+
+// import appStoreMobile from '../../assets/images/apple-store.png'
+// import googlePlayMobile from '../../assets/images/google-play.png'
+import appStoreMobile from '../../assets/images/home/whatis-applestore-mobile.svg'
+import appStoreDesktop from '../../assets/images/home/whatis-applestore-desktop.svg'
+import googlePlayMobile from '../../assets/images/home/whatis-googleplay-mobile.svg'
+import googlePlayDesktop from '../../assets/images/home/whatis-googleplay-desktop.svg'
 
 // import { IconButton } from '../reusable-components/'
 // import logoImg from '../../assets/images/home-what-is-logo.svg'
@@ -19,6 +34,10 @@ const StyledSection = styled.section`
     background: #191C23;
     // border: 2px solid red;
 
+    @media (min-width: 1024px) {
+        padding: 186px 0px 232px 160px;
+    }
+
     & > .blue-background {
         position: absolute;
         top: 0px;
@@ -26,41 +45,238 @@ const StyledSection = styled.section`
         width: 100%;
         height: 274px; 
         background: #416FF4;
+
+        @media (min-width: 1024px) {
+            position: absolute;
+            top: 0px;
+            left: 792px;
+            width: 100%;
+            height: 100%;
+        }
     }
 
-    & > .product-image {
+    & > .red-oval {
         position: absolute;
-        top: 64px;
-        // border: 1px dashed orange;
+        top: -20px;
+        left: -114px;
+        width: 260px;
+        height: 260px;
+        background-image:
+            url(${redOvalMobile});
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+        border: 1px dashed orange;
+    }
+
+    & > .dots {
+        position: absolute;
+        bottom: -61.5px;
+        left: -149px; 
+        width: 258px;
+        height: 123px;
+        background-image: 
+            url(${dotsMobile});
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+        border: 1px dashed orange;
+
+        @media (min-width: 1024px) {
+            position: absolute;
+            bottom: -41px;
+            left: -56px;
+        }
     }
 
     & > .stripes {
         position: absolute;
         top: 200px;
-        right: 0px;
-        // border: 1px dashed orange;
-    }
+        right: -36px;
+        width: 112px;
+        height: 176px;
+        background-image:
+            url(${stripesMobile});
+        border: 1px dashed orange;
 
-    & > .red-oval {
-        position: absolute;
-        top: -40px;
-        left: -114px;
-        // border: 1px dashed orange;
+        @media (min-width: 1024px) {
+            top: 596px;
+            right: 40px;
+            width: 176px;
+            height: 176px;
+            background-image:
+                url(${stripesDesktop});
+        }
     }
 
     & > .gray-oval {
-        width: 376px;
-        // width: 100%;
         position: absolute;
         top: 0px;
         left: calc(50% - 376px/2);
-        // border: 2px dashed white;
+        width: 376px;
+        height: 376px;
+        background-image:
+            url(${grayOvalMobile});
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+        border: 1px dashed orange;
+
+        @media (min-width: 1024px) {
+            top: 260px;
+            left: 727px;
+            width: 672px
+            height: 672px;
+            background-image:
+                url(${grayOvalDesktop});
+            border: 1px dashed green;
+        }
+    }
+
+    & > .product-image {
+        position: absolute;
+        // right: 0px;
+        top: 64px;
+        left: -20px;
+        width: 336px;
+        height: 268px;
+        background-image:
+            url(${productImageMobile});
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+        border: 1px dashed orange;
+
+        @media (min-width: 1024px) {
+            position: absolute;
+            top: 210px;
+            right: 0px;
+            width: 734px;
+            height: 583px;
+            background-image:
+                url(${productImageDesktop});
+        }
     }
 
     & > .header-container {
         margin-top: 120px;
+        display: flex;
+        flex-direction: column;
         color: white;
-        border: 2px dashed lightcoral;
+        border: 1px solid white;
+
+        @media (min-width: 1024px) {
+            // margin-left: 160px;
+            width: 496px;
+        }
+
+        & > .text {
+            font-family: DM Sans;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 40px;
+            line-height: 120%;
+            color: #FFFFFF;
+            border: 1px dashed orange;
+
+            @media (min-width: 1024px) {
+                font-size: 62px;
+            }
+        }
+
+        & > .body {
+            margin-top: 8px;
+            font-family: DM Sans;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 16px;
+            line-height: 140%;
+            color: #FFFFFF;
+            opacity: 0.7;
+            border: 1px dashed orange;
+        }
+
+       & > .app-buttons-wrapper {
+            margin-top: 32px;
+            display: flex;
+            gap: 12px;
+            border: 1px dashed orange;
+
+            & > .app-store-button {
+                width: 116px;
+                height: 42px;
+                background-image: 
+                    url(${appStoreMobile});
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: contain;
+                // border: 1px solid blue;
+
+                @media (min-width: 1024px) {
+                    width: 132px;
+                    height: 48px;
+                    background-image: 
+                        url${appStoreDesktop};
+                }
+           }
+
+           & > .google-play-button {
+                width: 116px;
+                height: 42px;
+                background-image: 
+                    url(${googlePlayMobile});
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: contain;
+                // border: 1px solid blue;
+
+                @media (min-width: 1024px) {
+                    width: 157px;
+                    height: 48px;
+                    background-image:
+                        url(${googlePlayDesktop});
+                }
+            }
+       }
+
+        & > .wrapper {
+            display: flex;
+            align-items: center;
+            border: 1px dashed orange;
+
+            & > .logo-container {
+                width: 164px;
+                height: 44px;
+                background-image:
+                    url(${logoMobile});
+                background-repeat: no-repeat;
+                background-position: center;
+                // background-size: contain;
+                // border: 1px dashed orange;
+
+                @media (min-width: 1024px) {
+                    width: 262px;
+                    height: 72px;
+                    background-image:
+                        url(${logoDesktop});
+                }
+            }
+
+            & > .question-mark {
+                margin-left: 4px;
+                font-family: DM Sans;
+                font-style: normal;
+                font-weight: bold;
+                font-size: 40px;
+                line-height: 120%;
+                color: #FFFFFF;
+                // border: 1px dashed orange;
+
+                @media (min-width: 1024px) {
+                    font-size: 62px;
+                }
+            }
+        }  
     }
 
     & > .benefits-container {
@@ -69,7 +285,13 @@ const StyledSection = styled.section`
         flex-direction: column;
         align-items: center;
         gap: 40px;
-        // border: 1px dashed lightblue;
+        border: 1px dashed lightblue;
+
+        @media (min-width: 1024px) {
+            // margin-left: 160px;
+            flex-direction: row;
+            width: 496px;
+        }
 
         & > .members-benefits {
             width: 100%;
@@ -82,38 +304,30 @@ const StyledSection = styled.section`
         }
     }
 `
+
 export default function WhatIsSection() {
 
     return (
         <StyledSection>
             <div className={'blue-background'}></div>
-            <img
-                src={grayOvalMobile}
-                alt={'temp alt'}
-                className={'gray-oval'}
-            />
+            <div className={'red-oval'}></div>
+            <div className={'dots'}></div>
+            <div className={'gray-oval'}></div>
+            <div className={'stripes'}></div>
+            <div className={'product-image'}></div>
 
-            <img
-                src={redOvalMobile}
-                alt={'temp alt'}
-                className={'red-oval'}
-            />
-
-            <img
-                src={backgroundStripesMobile}
-                alt={'temp alt'}
-                className={'stripes'}
-            />
-
-            <img
-                src={productImage}
-                alt={'temp alt'}
-                className={'product-image'}
-            />
 
             <div className={'header-container'}>
-                <h4>{'What is'}</h4>
-                <h4>{'?'}</h4>
+                <h4 className={'text'}>{'What is'}</h4>
+                <div className={'wrapper'}>
+                    <div className={'logo-container'}></div>
+                    <h4 className={'question-mark'}>{'?'}</h4>
+                </div>
+                <p className={'body'}>{'A dynamic and interactive online platform to empower minority professionals.'}</p>
+                <div className={'app-buttons-wrapper'}>
+                    <div className={'app-store-button'}></div>
+                    <div className={'google-play-button'}></div>
+                </div>
             </div>
 
             <div className={'benefits-container'}>
@@ -128,7 +342,7 @@ export default function WhatIsSection() {
                     color={'red'}
                     listTitle={'For Organizations'}
                     labels={['Jobs', 'Talent Scores', 'Systems & Services']}
-                    buttonLabel={'View Benefitssss'}
+                    buttonLabel={'View Benefits'}
                     className={'organizations-benefits'}
                 />
             </div>
