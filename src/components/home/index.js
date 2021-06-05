699f634a4b9d2246901a0cca37a1af3bf0281a07
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-05 11:52:50 -0500

    Commit

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import NetworkSection from './NetworkSection'
 import PlatformSection from './PlatformSection'
-import WhatIsSection from './WhatIsSection'
+// import WhatIsSection from './WhatIsSection'
 import LearnSection from './LearnSection'
 
 export default function HomePage() {
@@ -11,7 +11,7 @@ export default function HomePage() {
             <HeroSection />
             <NetworkSection />
             <PlatformSection />
-            <WhatIsSection />
+            {/* <WhatIsSection /> */}
             <LearnSection />
         </div>
     )
