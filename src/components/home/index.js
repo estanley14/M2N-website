0d3f8047d399451aaf3a1a1c8af0dcec64293f36
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-05 22:54:34 -0500

    Removed Section
    Removed WhatIfSection.js because Gatsby was not doing the hot-reload when changes were made to that file for some reason.
    
    Changes to other files trigger the hot re-load, but nothing at all happens when a  change is applied to this file.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import NetworkSection from './NetworkSection'
 import PlatformSection from './PlatformSection'
-import WhatIsSection from './WhatIsSection'
 import LearnSection from './LearnSection'
 
 export default function HomePage() {
@@ -11,7 +10,6 @@ export default function HomePage() {
             <HeroSection />
             <NetworkSection />
             <PlatformSection />
-            <WhatIsSection />
             <LearnSection />
         </div>
     )
