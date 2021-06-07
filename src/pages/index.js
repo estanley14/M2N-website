f36dd90e442520a99377b5a21bf71be3f6d8694c
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-07 05:00:22 -0500

    Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,24 +71,7 @@ import vector332x from "../import/img/vector-23@2x.png"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <HomePage />
-    {/* <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p> */}
-   {/* <input type="hidden" id="anPageName" name="page" value="home-tablet-720px" /> */}
-   
+    <HomePage />   
   </Layout>
 )
 
