e0bb77c466ce180019f94f1ebdec390281854b50
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-16 01:15:44 -0500

    Commit

--- src/components/landing/BenefitsSection.js
+++ src/components/landing/BenefitsSection.js
@@ -5,7 +5,7 @@ import mockupMobile from '../../assets/images/landing/benefits-image-mobile.png'
 import mockup2Mobile from '../../assets/images/landing/benefits-image2-mobile.png'
 
 const StyledSection = styled.section`
-    padding: 64px 16px 0px 16px;
+    padding: 64px 16px 48px 16px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -28,7 +28,7 @@ const Title = styled.h2`
 `
 
 const MockupImage = styled.img`
-    margin-bottom: 60px;
+    margin-bottom: 24px;
     border: 1px dashed green;
 `
 
@@ -46,7 +46,6 @@ const Header3 = styled.h3`
 `
 
 const ListWrapper = styled.div`
-    margin-bottom: 180px;
     display: flex;
     flex-direction: column;
     gap: 16px;
@@ -54,6 +53,21 @@ const ListWrapper = styled.div`
 `
 
 const Mockup2Image = styled.img`
+    margin-top: 184px;
+    margin-bottom: 24px;
+    border: 1px dashed green;
+`
+
+const PlatformText = styled.p`
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 140%;
+    text-align: center;
+    letter-spacing: -0.66px;
+    color: #FFFFFF;
+    opacity: 0.75;
     border: 1px dashed green;
 `
 
@@ -75,6 +89,14 @@ export default function BenefitsSection() {
                 src={mockup2Mobile}
                 alt={'product image'}
             />
+            <Header3>{'Opportunity'}</Header3>
+            <ListWrapper>
+                <EducationItem text={'Find employment within companies committed to supporting minority professionals'} />
+                <EducationItem text={'Accelerate your career through reciprocal mentor relationships'} />
+                <EducationItem text={'Engage in on-going dialogue and professional support within the MPower community'} />
+            </ListWrapper>
+            <Title>{'The MPower Platform Features'}</Title>
+            <PlatformText>{'Using proprietary technology MPower is a full scale platform for minority professionals.'}</PlatformText>
         </StyledSection>
     )
 }
