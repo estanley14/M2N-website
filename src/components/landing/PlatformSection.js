377ed694a4f6482d509fbb16675e21c7fca5efd9
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-16 02:41:56 -0500

    Commit

--- src/components/landing/PlatformSection.js
+++ src/components/landing/PlatformSection.js
@@ -3,6 +3,10 @@ import styled from 'styled-components'
 import PlatformCard from './PlatformCard'
 import newsIcon from '../../assets/images/landing/platform-news-icon.svg'
 import universityIcon from '../../assets/images/landing/platform-university-icon.svg'
+import keynoteIcon from '../../assets/images/landing/platform-keynote-icon.svg'
+import jobsIcon from '../../assets/images/landing/platform-jobs-icon.svg'
+import scoreIcon from '../../assets/images/landing/platform-score-icon.svg'
+import systemsIcon from '../../assets/images/landing/platform-systems-icon.svg'
 
 const StyledSection = styled.section`
     padding: 48px 16px 0px 16px;
@@ -37,22 +41,58 @@ const Subtitle = styled.p`
     border: 1px dashed green;
 `
 
+const CardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 42px;
+    border: 1px solid white;
+`
+
 export default function PlatformSection() {
     return (
         <StyledSection>
             <Title>{'The MPower Platform Features'}</Title>
             <Subtitle>{'Using proprietary technology MPower is a full scale platform for minority professionals.'}</Subtitle>
-            <PlatformCard
-                icon={newsIcon}
-                title={'News Feed'}
-                subtitle={'Knowledge and empowerment through daily delivery of curated stories and articles about Diversity, Equity and Inclusion.'}
-            />
-
-            <PlatformCard
-                icon={universityIcon}
-                title={'University'}
-                subtitle={'A video-based professional & career development curriculum. Online classes that empower, educate, & mentor minority professionals.'}
-            />
+            <CardWrapper>
+                <PlatformCard
+                    icon={newsIcon}
+                    title={'News Feed'}
+                    subtitle={'Knowledge and empowerment through daily delivery of curated stories and articles about Diversity, Equity and Inclusion.'}
+                />
+                <PlatformCard
+                    icon={universityIcon}
+                    title={'University'}
+                    subtitle={'A video-based professional & career development curriculum. Online classes that empower, educate, & mentor minority professionals.'}
+                />
+                <PlatformCard
+                    icon={keynoteIcon}
+                    title={'Keynote'}
+                    subtitle={'Unique tradecraft and life lesson discussions presented by globally recognized celebrities & thought leaders.'}
+                />
+                <PlatformCard
+                    icon={keynoteIcon}
+                    title={'Jobs'}
+                    subtitle={'Career advancement through M2N engagement with hiring organizations seeking to recruit diverse candidates.'}
+                />
+                <PlatformCard
+                    icon={jobsIcon}
+                    title={'Jobs'}
+                    subtitle={'Career advancement through M2N engagement with hiring organizations seeking to recruit diverse candidates.'}
+                />
+                <PlatformCard
+                    icon={scoreIcon}
+                    title={'Talent Score'}
+                    subtitle={'Talent scoring metrics for M2N internal recruiting based upon education, employment experience and activity within the platform.'}
+                />
+                <PlatformCard
+                    icon={systemsIcon}
+                    title={'Systems & Services'}
+                    subtitle={'Services to support new minority hires and the hiring organization to insure minority employees are engaged and contributing in their new role.​'}
+                />
+            </CardWrapper>
+            
+            
         </StyledSection>
     )
 }
